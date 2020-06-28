eb4a013588232862ed5fca0f9a1a5db7bab2bdbd
Marcel Kooi <1marcelkooi@gmail.com>
2020-06-27 23:23:30 -0400

    Install tailwindcss with purgecss

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
   return (
-    <Layout home>
+    <Layout home>>
       <Head>
         <title>{siteTitle}</title>
       </Head>
