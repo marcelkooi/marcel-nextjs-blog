abacd7dfec72794ff1b7ce48d0b97d0fd9b01421
Marcel Kooi <1marcelkooi@gmail.com>
2020-07-05 13:59:05 -0400

    Change back to individual markdown files for posts

--- lib/posts.ts
+++ lib/posts.ts
@@ -8,17 +8,13 @@ const postsDirectory = path.join(process.cwd(), 'posts')
 
 export function getSortedPostsData() {
   // Get file names under /posts
-  const posts = fs.readdirSync(postsDirectory)
-  const allPostsData: any[] = posts.map(post => {
-    const postDir = path.join(process.cwd(), 'posts', post)
-    const postFileNames = fs.readdirSync(postDir);
-    const markdownFileName = postFileNames.find((postFileName) => postFileName.match(/\.md$/)) || '';
+  const fileNames = fs.readdirSync(postsDirectory)
+  const allPostsData: any[] = fileNames.map(fileName => {
     // Remove ".md" from file name to get id
-    const id = markdownFileName.replace(/\.md$/, '')
+    const id = fileName.replace(/\.md$/, '')
 
     // Read markdown file as string
-    const fullPath = path.join(postsDirectory, post, markdownFileName)
-    console.log(fullPath);
+    const fullPath = path.join(postsDirectory, fileName)
     const fileContents = fs.readFileSync(fullPath, 'utf8')
 
     // Use gray-matter to parse the post metadata section
